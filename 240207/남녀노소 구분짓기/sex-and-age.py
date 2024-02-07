bc650b0d9c397eb628c86bd5508d0aa9ee1ef0d3
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-02-07 17:24:11 +0900

    Title: 남녀노소 구분짓기, Time: 153ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240207/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,12 @@
+sex, age = int(input()), int(input())
+
+if age < 19 :
+    if sex == 0 :
+        print("BOY")
+    else :
+        print("GIRL")
+else :
+    if sex == 0 :
+        print("MAN")
+    else :
+        print("WOMAN")
