b43e7565f3086598abbe6c6b45dc7fdb5153667e
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-02-07 17:18:48 +0900

    Title: 두 사람, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240207/두 사람/two-person.py
+++ 240207/두 사람/two-person.py
@@ -4,7 +4,7 @@ arrB = input().split()
 aAge, aSex = int(arrA[0]), arrA[1]
 bAge, bSex = int(arrB[0]), arrB[1]
 
-if (aAge >= 19 and aSex == "M") or (bAge >= 19 and bSex == "W") :
+if (aAge >= 19 and aSex == "M") or (bAge >= 19 and bSex == "M") :
     print(1)
 else :
     print(0)
