4e4957eb23f1a2eac64d5adf2908bab52a9351bf
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-02-07 13:43:09 +0900

    Title: 점수 비교, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240207/점수 비교/score-comparison.py
+++ 240207/점수 비교/score-comparison.py
@@ -1,7 +1,7 @@
 arrA = list(map(int, input().split()))
 arrB = list(map(int, input().split()))
 
-aMath, aMath = arrA[0], arrA[1]
+aMath, aEng = arrA[0], arrA[1]
 bMath, bEng = arrB[0], arrB[1]
 
 if aMath > bMath and aEng > bEng :
