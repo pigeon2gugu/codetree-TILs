a6e894a606813fe94735441b26c215c10cee24f2
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-02-07 13:04:47 +0900

    Title: 알파벳에 따른 평가, Time: 229ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240207/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+upper = input()
+
+if upper == "S" :
+    print("Superior")
+elif upper == "A" :
+    print("Excellent")
+elif upper == "B" :
+    print("Good")
+elif upper == "C" :
+    print("Usually")
+elif upper == "D" :
+    print("Effort")
+else :
+    print("Failure")
