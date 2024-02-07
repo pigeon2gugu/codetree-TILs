c307912988c8594cff68ba1580f3e2832e393009
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-02-07 13:34:43 +0900

    Title: 비교 연산, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240207/비교 연산/comparison-operator.py
@@ -0,0 +1,33 @@
+arr = list(map(int, input().split()))
+a, b = arr[0], arr[1]
+
+if (a>=b) :
+    print(1)
+else :
+    print(0)
+
+if (a>b) :
+    print(1)
+else :
+    print(0)
+
+if (b>=a) :
+    print(1)
+else :
+    print(0)
+
+
+if (b>a) :
+    print(1)
+else :
+    print(0)
+
+if (a==b) :
+    print(1)
+else :
+    print(0)
+
+if (a!=b) :
+    print(1)
+else :
+    print(0)
