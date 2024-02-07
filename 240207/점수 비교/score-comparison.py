c8db6628e7ad6950a7864bebfd0a20ae9578c0c1
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-02-07 13:39:30 +0900

    Title: 점수 비교, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240207/점수 비교/score-comparison.py
@@ -0,0 +1,10 @@
+math = list(map(int, input().split()))
+eng = list(map(int, input().split()))
+
+aMath, bMath = math[0], math[1]
+aEng, bEng = eng[0], eng[1]
+
+if aMath > bMath and aEng > bEng :
+    print(1)
+else :
+    print(0)
