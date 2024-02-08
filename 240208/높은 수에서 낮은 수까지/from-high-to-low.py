cfe6ea460f2a035d09f69644b7d2c7027bd48168
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-02-08 09:50:27 +0900

    Title: 높은 수에서 낮은 수까지, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240208/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,10 @@
+arr = list(map(int, input().split()))
+
+a, b = arr[0], arr[1]
+
+if (a >= b) :
+    for i in range(a, b-1, -1) :
+        print(i, end = " ")
+else :
+    for i in range(b, a-1, -1) :
+        print(i, end = " ")
