19fa6ec6565055e24d2cf88697e08488886f3ebf
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-02-21 16:36:13 +0900

    Title: 구구단 만들기 2, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240221/구구단 만들기 2/print-multiplication-table-2.py
@@ -0,0 +1,9 @@
+arr = list(map(int, input().split()))
+a, b = arr[0], arr[1]
+
+for i in range(2, 10, 2) :
+    for j in range(b, a-1, -1) :
+        print(f"{j} * {i} = {i*j}", end = " ")
+        if j > a :
+            print("/", end = " ")
+    print()
