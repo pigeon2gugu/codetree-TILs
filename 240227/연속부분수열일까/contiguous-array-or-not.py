9673a147196d793d660421141012e52c27f5dafc
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-02-27 16:51:51 +0900

    Title: 연속부분수열일까, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240227/연속부분수열일까/contiguous-array-or-not.py
+++ 240227/연속부분수열일까/contiguous-array-or-not.py
@@ -3,22 +3,32 @@ arrA = list(map(int, input().split()))
 arrB = list(map(int, input().split()))
 
 
-idx = -1
+firstIdx = []
 tf = "Yes"
-for elem in arrB :
 
-    if elem in arrA :
-        temp = arrA.index(elem)
-        arrA[temp] = -1
-    else :
-        tf = "No"
-        break
+for i, elem in enumerate(arrA) :
+    if elem == arrB[0] :
+        firstIdx.append(i)
 
-    if idx != -1 and temp != idx + 1 :
-        tf = "No"
-        break
+if len(firstIdx) == 0 :
+    tf = "No"
 
-    idx = temp
+else :
+    for elem in firstIdx :
+        idx = elem
 
+        for elemB in arrB :
+            if idx + 1 > len(arrA) - 1 :
+                tf = "No"
+                break 
+
+            if arrA[idx+1] != elemB :
+                tf = "No"
+                break
+
+            tf = "Yes"
+        
+        if tf == "Yes" :
+            break
 
 print(tf)
