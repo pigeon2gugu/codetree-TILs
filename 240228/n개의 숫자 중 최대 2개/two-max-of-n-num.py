1ce8546b7630b90229e1aff82fbda936be21781b
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-02-28 13:37:19 +0900

    Title: n개의 숫자 중 최대 2개, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240228/n개의 숫자 중 최대 2개/two-max-of-n-num.py
+++ 240228/n개의 숫자 중 최대 2개/two-max-of-n-num.py
@@ -4,7 +4,7 @@ arr = list(map(int, input().split()))
 maxVal = max(arr)
 maxValIdx = arr.index(max(arr))
 
-maxVal2 = sys.minsize
+maxVal2 = -sys.maxsize
 for i, elem in enumerate(arr) :
     if i == maxValIdx :
         continue
