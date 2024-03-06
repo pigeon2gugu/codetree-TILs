1de8fff1ad015574bc585cf794b6cd83977471f7
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-03-06 18:02:06 +0900

    Title: 사칙연산 함수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240306/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,33 @@
+a, o, c = tuple(input().split())
+a = int(a)
+c = int(c)
+
+def addCalculator(a, b) :
+    return a + b
+
+def diffCalculator(a, b) :
+    return a - b
+
+def multipleCaculator(a, b) :
+    return a * b
+
+def divCalculator(a, b) :
+    return a // b
+
+
+def calculator(a, o, c) :
+
+    expression = f"{a} {o} {c} = "
+    if o == "+" :
+        return expression + str(ddCalculator(a, c))
+    elif o == "-" :
+        return expression + str(diffCalculator(a, c))
+    elif o == "*" :
+        return expression + str(multipleCaculator(a, c))
+    elif o == "/" :
+        return expression + str(divCalculator(a, c))
+    else :
+        return "False"
+
+
+print(calculator(a, o, c))
