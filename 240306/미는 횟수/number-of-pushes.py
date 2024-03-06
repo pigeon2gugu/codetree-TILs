50a4b71f63a245326ff8ea2634a818359ee2a8bf
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-03-06 16:22:46 +0900

    Title: 미는 횟수, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240306/미는 횟수/number-of-pushes.py
+++ 240306/미는 횟수/number-of-pushes.py
@@ -3,10 +3,17 @@ b = input()
 
 cnt = 0
 while True :
+
     if a == b :
+        break
+
+    if cnt == len(a) :
         break
 
     a = a[-1] + a[:-1]
     cnt += 1
 
-print(cnt)
+if cnt == len(a) :
+    print(-1)
+else :
+    print(cnt)
