210d1f82989852b1b0785a6f04ed205ba967e545
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-03-08 18:01:53 +0900

    Title: 2개 이상의 알파벳, Time: 130ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240308/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240308/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -5,11 +5,18 @@ def hasDuplicateAlpha(string) :
     if len(string) <= 1 :
         return False
 
-    firstAlpha = string[0]
-    if firstAlpha in string[1:] :
-        return False
+    temp = string[0]
+    cnt = 1
+    for i in range(1, len(string)-1) :
+        if temp not in string[i:-1] :
+            cnt += 1
+
+        if cnt == 2 :
+            return True
+
+        temp = string[i]
     
-    return True
+    return False
 
 
 if hasDuplicateAlpha(s) :
