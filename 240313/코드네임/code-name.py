364d81a5b6a9ffa87701ca2fc8afdaa823778eed
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-03-13 17:30:57 +0900

    Title: 코드네임, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240313/코드네임/code-name.py
@@ -0,0 +1,20 @@
+class Spy :
+    def __init__(self, codeName = "", score = 0) :
+        self.codeName = codeName
+        self.score = score
+
+spys = []
+for _ in range(5) :
+    codeName, score = tuple(input().split())
+    spy = Spy(codeName, int(score))
+    spys.append(spy)
+
+minScore = spys[0].score
+idx = 0
+
+for i in range(1, len(spys)) :
+    if spys[i].score < minScore :
+        minScore = spys.score
+        idx = i
+
+print(spys[idx].codeName + " " + str(spys[idx].score))
