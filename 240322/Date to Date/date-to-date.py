04db05dce46266917e53dfb0f9e489320bc1943d
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-03-22 15:10:05 +0900

    Title: Date to Date, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240322/Date to Date/date-to-date.py
+++ 240322/Date to Date/date-to-date.py
@@ -2,12 +2,9 @@ m1, d1, m2, d2 = tuple(map(int, input().split()))
 
 dayOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-m1Date = sum(dayOfMonth[:m1]) + d1
-m2Date = sum(dayOfMonth[:m2]) + d2
+m1Date = sum(dayOfMonth[:m1-1]) + d1
+m2Date = sum(dayOfMonth[:m2-1]) + d2
 
-diffDate = m2Date - m1Date
+diffDate = m2Date - m1Date + 1
 
-if diffDate == 0 :
-    print(1)
-else :
-    print(diffDate)
+print(diffDate)
