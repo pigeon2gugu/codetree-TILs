7f29d710650b7678003c7b1de209046e29f37657
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-03-22 15:23:31 +0900

    Title: 그 요일은, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240322/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,17 @@
+m1, d1, m2, d2 = tuple(map(int, input().split()))
+day = input()
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+def getDate(m, d) :
+    dayOfMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    return sum(dayOfMonth[:m-1]) + d
+
+cnt = (getDate(m2, d2) - getDate(m1, d1)) // 7
+
+if days[(getDate(m2, d2) - getDate(m1, d1)) % 7] == day :
+    cnt += 1
+
+
+print(cnt)
