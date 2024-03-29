fc9e39be5106ba1963bf48dbda7671b8f7cbda2a
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-03-29 11:42:03 +0900

    Title: 벌금은 누구에게, Time: 144ms, Memory: 25MB, Status: Passed - CodeTree

--- 240329/벌금은 누구에게/who-will-pay.py
+++ 240329/벌금은 누구에게/who-will-pay.py
@@ -1,10 +1,15 @@
 n, m, k = tuple(map(int, input().split()))
 panalties = [0] * (n+1)
 
+cnt = 0
 for _ in range(m) :
     idx = int(input())
     panalties[idx] += 1
 
     if panalties[idx] >= k :
         print(idx)
+        cnt += 1
         break
+
+if cnt == 0 :
+    print(-1)
