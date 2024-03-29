db17363c9282a2d032f402290e74ec59cf5ad1bd
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-03-29 15:40:30 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240329/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240329/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,21 +1,22 @@
 n, k, p, t = tuple(map(int, input().split()))
 arr = [tuple(map(int, input().split())) for _ in range(t)]
 attacked = [0] * (n + 1)
+cnts = [0] * (n + 1)
+
 attacked[p] = 1
 
 arr.sort(key = lambda x : x[0])
 
-cnt = 0
+
 for _ , (time, x, y) in enumerate(arr) :
 
-    if cnt >= k :
-        break
-
-    if x == p or y == p and cnt < k:
-        cnt += 1
-
-        attacked[x] = 1
+    if attacked[x] == 1 and cnts[x] < p :
+        cnts[x] += 1
         attacked[y] = 1
+
+    if attacked[y] == 1 and cnts[y] < p :
+        cnts[y] += 1
+        attacked[x] = 1
 
 
 for a in attacked[1::] :
