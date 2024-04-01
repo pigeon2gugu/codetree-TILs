52cf7c106cce916522dde166e19bcd3c172c6ac2
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-04-01 10:35:31 +0900

    Title: 문자에 따른 명령 2, Time: 153ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240401/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,21 @@
+l = input()
+dx = [1, 0, -1, 0]
+dy = [0, -1, 0, 1]
+
+x, y = 0, 0
+direction = 3
+distance = 0
+
+for ll in l :
+    if ll == 'R' :
+        direction += 1
+    elif ll == 'L' :
+        direction -= 1
+    elif ll == 'F' :
+        distance += 1
+
+
+x += dx[direction % 4] * distance
+y += dy[direction % 4] * distance
+
+print(x, y)
