8f1d9735633743deca0cc0b76b6d3e761f7b90d5
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-04-02 14:58:31 +0900

    Title: 작은 구슬의 이동, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240402/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,26 @@
+n, t = tuple(map(int, input().split()))
+y, x, d = tuple(input().split())
+
+x = int(x) - 1
+y = int(y) - 1
+
+directions = {'U' : 2, 'D' : 1, 'R' : 3, 'L' : 0}
+
+dx, dy = [-1, 0, 0, 1], [0, -1, 1, 0]
+
+def inRange(x, y) :
+    return x >= 0 and x < n and y >= 0 and y < n
+
+d = directions[d]
+nx, ny = x, y
+
+for tt in range(t + 1) :
+
+    if not inRange(nx, ny) :
+        d = 3 - d
+
+    nx = nx + dx[d]
+    ny = ny + dy[d]
+
+
+print(ny + 1, nx + 1)
