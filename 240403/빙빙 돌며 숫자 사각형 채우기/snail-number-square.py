c0379ff7e16a9866ec98d970aacaadc62234d75e
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-04-03 16:18:24 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240403/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -0,0 +1,27 @@
+n, m = tuple(map(int, input().split()))
+answer = [[0] * m for _ in range(n)]
+
+dc, dr = [1, 0, -1, 0], [0, 1, 0, -1]
+r, c = 0, 0
+
+def inRange(r, c) :
+    return r >= 0 and r < n and c >= 0 and c < m
+
+direction = 0
+for i in range(1, n * m + 1) :
+
+    answer[r][c] = i
+
+    nr = r + dr[direction]
+    nc = c + dc[direction]
+
+    if not inRange(nr, nc) or answer[nr][nc] != 0 :
+        direction = (direction + 1) % 4
+    
+    r = r + dr[direction]
+    c = c + dc[direction]
+
+for i in range(m) :
+    for j in range(n) :
+        print(answer[i][j], end = " ")
+    print()
