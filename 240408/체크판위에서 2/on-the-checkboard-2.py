e349dd12842d0f965b47eb92202b15b1422224a2
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-04-08 17:08:34 +0900

    Title: 체크판위에서 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240408/체크판위에서 2/on-the-checkboard-2.py
+++ 240408/체크판위에서 2/on-the-checkboard-2.py
@@ -6,11 +6,11 @@ cnt = 0
 start = arr[0][0]
 end = arr[-1][-1]
 
-for i in range(1, len(arr)-1) :
-    for j in range(1, len(arr)-1) :
+for i in range(1, r-1) :
+    for j in range(1, c-1) :
         if arr[i][j] != start :
-            for k in range(i+1, len(arr) - 1) :
-                for l in range(j+1, len(arr) - 1) :
+            for k in range(i+1, r - 1) :
+                for l in range(j+1, c - 1) :
                     if arr[k][l] != end :
                         cnt+=1
 
