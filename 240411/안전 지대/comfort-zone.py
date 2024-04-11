d10f0a417cb195f0243ff7dc1bf226fc2c606e9d
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-04-11 22:13:56 +0900

    Title: 안전 지대, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240411/안전 지대/comfort-zone.py
@@ -0,0 +1,37 @@
+n, m = tuple(map(int, input().split()))
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+areas = []
+
+def inRange(x, y):
+    return 0 <= x < n and 0 <= y < m 
+
+def canMove(x, y, k):
+    return inRange(x, y) and graph[x][y] > k and not visited[x][y]
+
+def dfs(x, y, k):
+    visited[x][y] = True
+
+    dxs, dys = [0, -1, 0, 1], [1, 0, -1, 0]
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if canMove(nx, ny, k):
+            dfs(nx, ny, k)
+
+
+maxHeight = max(max(row) for row in graph)
+for h in range(1, maxHeight + 1):
+    visited = [[False for _ in range(m)] for _ in range(n)]
+    cnt = 0
+    for i in range(n):
+        for j in range(m):
+            if canMove(i, j, h):
+                cnt += 1
+                dfs(i, j, h)
+
+    areas.append(cnt)
+
+maxCount = max(areas)
+maxIndex = areas.index(maxCount) + 1
+
+print(maxCount, maxIndex)
