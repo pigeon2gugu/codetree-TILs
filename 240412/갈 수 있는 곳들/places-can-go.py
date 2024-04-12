b16d9312b9612347af1b49ec2d78c37017d72507
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-04-12 10:39:15 +0900

    Title: 갈 수 있는 곳들, Time: 296ms, Memory: 27MB, Status: Passed - CodeTree

--- 240412/갈 수 있는 곳들/places-can-go.py
+++ 240412/갈 수 있는 곳들/places-can-go.py
@@ -1,13 +1,9 @@
 from collections import deque
-import sys
-
-sys.setrecursionlimit(10000)
 
 n, k = tuple(map(int, input().split()))
 graph = [list(map(int, input().split())) for _ in range(n)]
 points = [tuple(map(int, input().split())) for _ in range(k)] 
 visited = [[False for _ in range(n)] for _ in range(n)]
-cnt = 0
 
 q = deque()
 
@@ -18,27 +14,28 @@ def canMove(x, y) :
     return inRange(x, y) and graph[x][y] == 0 and visited[x][y] == False
 
 def bfs() :
-    global cnt
     while q :
         x, y = q.popleft()
-        visited[x][y] = True
         
         dxs, dys = [0, 1, 0 ,-1], [1, 0, -1, 0]
         for dx, dy in zip(dxs, dys) :
             nx, ny = x + dx, y + dy
             if canMove(nx, ny) :
                 q.append((nx, ny))
-                cnt += 1
-                bfs()
-
+                visited[nx][ny] = True
 
 for i in range(len(points)) :
     x, y = points[i][0]-1, points[i][1]-1
-
-    if not visited[x][y] :
-        cnt += 1
-
     q.append((x, y))
-    bfs()
+    visited[x][y] = True
+
+bfs()
+
+cnt = sum([
+    1
+    for x in range(n)
+    for y in range(n)
+    if visited[x][y]
+])
 
 print(cnt)
