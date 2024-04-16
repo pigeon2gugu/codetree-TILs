ba86411d1e1b91589cacb031fe28d42c12385ed9
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-04-16 18:00:35 +0900

    Title: Carry 피하기 2, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240416/Carry 피하기 2/escaping-carry-2.py
+++ 240416/Carry 피하기 2/escaping-carry-2.py
@@ -3,10 +3,13 @@ nums = [input() for _ in range(n)]
 
 maxNum = 0
 for i in range(len(nums) - 1) :
+
     temp = int(nums[i])
+    cnt = 0
     
     for j in range(i + 1, len(nums)) :
         isCarry = False
+        
         for k in range(min(len(str(temp)), len(nums[j]))) :
             if int(str(temp)[len(str(temp)) - k - 1]) + int(nums[j][len(nums[j]) - k - 1]) > 9 :
                 isCarry = True
@@ -14,7 +17,12 @@ for i in range(len(nums) - 1) :
         
         if not isCarry : 
             temp += int(nums[j])
+            cnt += 1
 
-    maxNum = max(maxNum, temp)
+    if cnt > 0 :
+        maxNum = max(maxNum, temp)
 
-print(maxNum)
+if maxNum == 0 :
+    print(-1)
+else :
+    print(maxNum)
