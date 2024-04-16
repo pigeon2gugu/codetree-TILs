a78a355d080ce1906695bb8b0ec33e3db8ea22f8
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-04-16 15:39:00 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240416/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -0,0 +1,10 @@
+s = input()
+
+cnt = 0
+for i in range(len(s) - 1) :
+    if s[i] == '(' and s[i+1] == '(' :
+        for j in range(i+1, len(s)-1) :
+            if s[j] == ')' and s[j+1] == ')' :
+                cnt += 1
+
+print(cnt)
