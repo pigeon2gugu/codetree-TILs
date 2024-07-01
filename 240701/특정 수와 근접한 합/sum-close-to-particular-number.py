d6f6c07337c0b8036bfa1d63943acda1dd20bded
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-07-01 13:02:54 +0900

    Title: 특정 수와 근접한 합, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240701/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240701/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -1,22 +1,14 @@
 import sys
-sys.setrecursionlimit(100000)
 
 n, s = map(int, input().split())
 arr = list(map(int, input().split()))
 
 min_diff = sys.maxsize
 
-def find_min_diff(arr, n, s, selected, start):
-    global min_diff
-    if len(selected) == n-2:
-        current_sum = sum(selected)
-        current_diff = abs(s - current_sum)
+for i in range(n):
+    for j in range(i + 1, n):
+        remaining_sum = sum(arr) - arr[i] - arr[j]
+        current_diff = abs(s - remaining_sum)
         min_diff = min(min_diff, current_diff)
-        return
-
-    for i in range(start, len(arr)):
-        find_min_diff(arr, n, s, selected + [arr[i]], i + 1)
-
-find_min_diff(arr, n, s, [], 0)
 
 print(min_diff)
