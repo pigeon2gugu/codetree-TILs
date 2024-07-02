417f1f628057a611e777f924cd967bf8ab636c6a
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-07-02 14:26:02 +0900

    Title: 아름다운 수열 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240702/아름다운 수열 2/beautiful-sequence-2.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+subArr = list(map(int, input().split()))
+ans = 0
+
+for i in range(n - m + 1) :
+    cnt = 0
+    for j in range(i, m + 1) :
+        for k in range(m) :
+            if arr[j] == subArr[k] :
+                cnt += 1
+
+        if cnt == m :
+            ans += 1
+            break
+
+
+print(ans)
