8e6583c333836a445c68bac0983698cd409e94f0
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-07-09 15:17:33 +0900

    Title: G or H 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240709/G or H 2/G-or-H-2.py
@@ -0,0 +1,31 @@
+n = int(input())
+arr = ["n/a"] * 100
+points = []
+
+for _ in range(n):
+    info = input().split()
+    point = int(info[0])
+    alpha = info[1]
+
+    arr[point] = alpha
+    points.append(point)
+
+points = sorted(points)
+maxVal = 0
+
+for i in range(n - 1):
+    tempPoint = 0
+    gCount = 0
+    hCount = 0
+    for j in range(i, n):
+        if arr[points[j]] == 'G':
+            gCount += 1
+        elif arr[points[j]] == 'H':
+            hCount += 1
+        
+        if gCount == hCount or gCount == 0 or hCount == 0:
+            tempPoint = j
+
+    maxVal = max(maxVal, points[tempPoint] - points[i])
+
+print(maxVal)
