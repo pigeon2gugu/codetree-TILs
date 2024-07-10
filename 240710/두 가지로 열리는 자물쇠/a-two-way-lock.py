715d2e1037a3f0d02179cb01919a413e229021aa
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-07-10 13:28:38 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 470ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240710/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -0,0 +1,29 @@
+n = int(input())
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+ans = 0
+
+def isNear(a, b) :
+    if (abs(a - b) <= 2 or abs(a - b) >= 8) :
+        return True
+    
+    return False
+
+
+def isOpen(i, j, k) :
+    if isNear(arr1[0], i) and isNear(arr1[1], j) and isNear(arr1[2], k) :
+        return True
+    
+    if isNear(arr2[0], i) and isNear(arr2[1], j) and isNear(arr2[2], k) :
+        return True
+
+    return False
+
+
+for i in range(1, n + 1) :
+    for j in range(1, n + 1) :
+        for k in range(1, n + 1) :
+            if isOpen(i, j, k) :
+                ans += 1
+
+print(ans)
