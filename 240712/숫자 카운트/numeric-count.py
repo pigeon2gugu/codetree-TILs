7bb948b39440f827cee15e6924b20c860edaab7f
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-07-12 14:04:12 +0900

    Title: 숫자 카운트, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240712/숫자 카운트/numeric-count.py
+++ 240712/숫자 카운트/numeric-count.py
@@ -8,8 +8,8 @@ for _ in range(n):
     queries.append((query, int(count1), int(count2)))
 
 for i in range(1, 10) :
-    for j in range(10) :
-        for k in range(10) :
+    for j in range(1, 10) :
+        for k in range(1, 10) :
             number = i * 100 + j * 10 + k
             numbers.append(str(number))
 
