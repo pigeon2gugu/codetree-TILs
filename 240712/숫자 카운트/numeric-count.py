e6aca0a4f4b2f4f7a73a8b4d2fd7423d4a6067a0
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-07-12 14:03:11 +0900

    Title: 숫자 카운트, Time: 127ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240712/숫자 카운트/numeric-count.py
@@ -0,0 +1,33 @@
+n = int(input())
+queries = []
+numbers = []
+ans = 0
+
+for _ in range(n):
+    query, count1, count2 = input().split()
+    queries.append((query, int(count1), int(count2)))
+
+for i in range(1, 10) :
+    for j in range(10) :
+        for k in range(10) :
+            number = i * 100 + j * 10 + k
+            numbers.append(str(number))
+
+def calculateCounts(a, b):
+    count1 = sum(1 for i in range(3) if a[i] == b[i])
+    count2 = sum(1 for i in range(3) if a[i] != b[i] and a[i] in b)
+    return count1, count2
+
+for number in numbers:
+    valid = True
+    for query, count1, count2 in queries:
+        calculatedCount1, calculatedCount2 = calculateCounts(number, query)
+
+        if count1 != calculatedCount1 or count2 != calculatedCount2:
+            valid = False
+            break
+
+    if valid:
+        ans += 1
+    
+print(ans)
