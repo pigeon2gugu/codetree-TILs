24f3e1939c75431aa8f91b37334369a8e58ae9b8
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-07-23 15:06:25 +0900

    Title: 개발팀의 능력, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240723/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,32 @@
+import sys
+n = 5
+arr = list(map(int, input().split()))
+ans = sys.maxsize
+
+def getDiff(i, j, k, l) :
+    point1 = arr[i] + arr[j]
+    point2 = arr[k] + arr[l]
+    point3 = sum(arr) - point1 - point2
+
+    if point1 == point2 or point1 == point3 or point2 == point3 :
+        return sys.maxsize
+
+    maxPoint = max(max(point1, point2), point3)
+    minPoint = min(min(point1, point2), point3)
+
+    return (maxPoint - minPoint)
+
+for i in range(n-1) :
+    for j in range(i, n) :
+        for k in range(n-1) :
+            for l in range(k, n) :
+                if i == k or i == l or j == k or j == l :
+                    continue
+
+                ans = min(ans, getDiff(i, j, k, l))
+
+
+if ans == sys.maxsize :
+    ans = -1
+
+print(ans)
