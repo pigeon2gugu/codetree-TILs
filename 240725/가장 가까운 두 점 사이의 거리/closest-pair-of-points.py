c9ca81a12917375926cdd930c4127c3b001db2fc
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-07-25 16:19:05 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240725/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(input())
+arr = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+ans = sys.maxsize()
+
+for i in range(n) :
+    for j in range(n) :
+        if i == j :
+            continue
+
+        ans = min(ans, ans = (arr[i][0] - arr[j][0])^2 + (arr[i][1] - arr[j][1])^2)
+
+print(ans)
