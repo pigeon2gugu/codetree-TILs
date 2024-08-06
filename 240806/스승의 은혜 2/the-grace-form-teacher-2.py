db22c4250092321b7cedb4b7f21f015c172ac343
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-08-06 10:39:29 +0900

    Title: 스승의 은혜 2, Time: 181ms, Memory: 25MB, Status: Passed - CodeTree

--- 240806/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240806/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -1,17 +1,30 @@
-from itertools import combinations
-
 n, b = tuple(map(int, input().split()))
 arr = [int(input()) for _ in range(n)]
+arr.sort()
+
 ans = 0
 
-for r in range(1, n + 1):
-    for combo in combinations(range(n), r):
-        total_price = sum(arr[i] for i in combo)
-            
-        for i in combo:
-            discounted_price = total_price - arr[i] // 2
-            if discounted_price <= b:
-                ans = max(ans, r)
-                break
+for i in range(n):
+    tempSum = arr[i] / 2
+    cnt = 0
+
+    if tempSum > b:
+        continue
+
+    cnt = 1
+    for j in range(n):
+        if i == j:
+            continue
+
+        if tempSum + arr[j] > b :
+            continue
+
+        cnt += 1
+        tempSum += arr[j]
+
+    if tempSum > b :
+        cnt -= 1
+    
+    ans = max(ans, cnt)
 
 print(ans)
