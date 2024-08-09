6862f4daf538f90ad81465731622bb047488ccc2
하늘 <111864831+pigeon2gugu@users.noreply.github.com>
2024-08-09 17:04:49 +0900

    Title: 흥미로운 숫자 2, Time: 227ms, Memory: 25MB, Status: Passed - CodeTree

--- 240809/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240809/흥미로운 숫자 2/interesting-numbers-2.py
@@ -8,12 +8,16 @@ for i in range(x, y + 1):
     for digit in num_str:
         temp[int(digit)] += 1
 
-    cnt = 0
+    cnt1 = 0
+    cnt2 = 0
     for count in temp:
         if count == 1:
-            cnt += 1
+            cnt1 += 1
 
-    if cnt == 1:
+        if count > 0 :
+            cnt2 += 1
+
+    if cnt1 == 1 and cnt2 == 2:
         ans += 1
 
 print(ans)
